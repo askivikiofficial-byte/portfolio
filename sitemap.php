48420806e2e9b638fd424aff79a9f10203544c19
Ask I Viki <askiviki.official@gmail.com>
2026-01-09 04:44:45 +0530

    Sitemap issue fix

--- sitemap.php
+++ sitemap.php
@@ -20,7 +20,7 @@ $xml .= '<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EO
 
 foreach ($pages as $page) {
     $xml .= "  <url>" . PHP_EOL;
-    $xml .= "    <loc>{$domain}/{$page}</loc>" . PHP_EOL;
+    $xml .= "    <loc>{$domain}{$page}</loc>" . PHP_EOL;
     $xml .= "    <lastmod>" . date('Y-m-d') . "</lastmod>" . PHP_EOL;
     $xml .= "    <changefreq>weekly</changefreq>" . PHP_EOL;
     $xml .= "    <priority>0.8</priority>" . PHP_EOL;
